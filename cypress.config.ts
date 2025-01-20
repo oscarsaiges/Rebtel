98bad95d10ad948d18d53960934077468f07907a
Oscar Saiges <oscar.saiges@ipiccolo.com>
2025-01-20 21:02:21 +0100

    Adding Mochawesome to the CI/CD

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+  reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: true,
+      html: true,
+      json: true,
+      timestamp: 'YYYYMMDDHHmmss',
+      charts: true,
+      reportPageTitle: 'Cypress Test Report',
     },
   },
 });
